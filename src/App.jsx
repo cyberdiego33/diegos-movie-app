19c0a197ef97bd5ae59a60082786b0f9bc35189e
Diego <iwujigoodluck5@gmail.com>
2025-08-16 21:57:34 +0000

    set a working api

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,56 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { API_BASE_URL, API_KEY, API_OPTIONS } from "./config";
+import Search from "./components/Search";
 
-import './App.css'
+const App = function () {
+  const [searchTerm, setSearchTerm] = useState("Search movies");
+  const [errormessage, setErrorMessage] = useState(null);
 
-const App = function() {
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  const fetchMovies = async function () {
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+      const response = await fetch(endpoint, API_OPTIONS);
+      
+      if (!response.ok) throw new Error("network error");
+
+      const data = await response.json();
+
+      alert("response successful");
+    } catch (error) {
+      console.log("Failed to fetch: ", error);
+      setErrorMessage("Error fetching message; please try again");
+    }
+  };
 
   return (
-    <>
-      <h1 className='bg-red-500'> My movie Application </h1>
-    </>
-  )
-}
+    <main>
+      <div className="pattern" />
 
-export default App
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="hero background image" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> you'll enjoy
+            without hassles
+          </h1>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+
+        <section className="all-movies">
+          <h2 className="">All movies</h2>
+
+          {errormessage && <p className="text-red-500">{errormessage}</p>}
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/config.js
@@ -0,0 +1,11 @@
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+    method: "GET",
+    headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${API_KEY}`
+    }
+}
+
+export { API_BASE_URL, API_KEY, API_OPTIONS }
